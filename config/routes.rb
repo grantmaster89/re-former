d4fed4a798294ebfac2cea08f88f636b464e8ef8
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-04-30 15:20:13 -0700

    using form for helper in users views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Routes go here
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :edit, :update]
 end
